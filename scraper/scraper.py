d3ece27bdc4ea8c50fe25f5f632f6dc37806c446
DERPY-XPS\derpy <jonathan.n@u.nus.edu>
2019-02-16 18:15:43 +0800

    updated scraper to be able to do get URLs from page

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,28 @@
 import json, requests
 
-r = requests.get(https://sprs.parl.gov.sg/search/home)      
+url = "https://sprs.parl.gov.sg/search/searchResult"
+
+# TODO: Automate function to get all
+
+querystring = {"":""}
+
+payload = "{\n    \"keyword\": \"undefined\",\n    \"fromday\": \"16\",\n    \"frommonth\": \"02\",\n    \"fromyear\": \"2019\",\n    \"today\": \"16\",\n    \"tomonth\": \"02\",\n    \"toyear\": \"2019\",\n    \"dateRange\": \"* TO NOW\",\n    \"reportContent\": \"with all the words\",\n    \"parliamentNo\": \"\",\n    \"selectedSort\": \"date_dt desc\",\n    \"portfolio\": \"undefined\",\n    \"mpName\": \"\",\n    \"rsSelected\": \"\",\n    \"lang\": \"\",\n    \"startIndex\": \"0\",\n    \"endIndex\": \"19\",\n    \"titleChecked\": \"false\",\n    \"ministrySelected\": \"\"\n}"
+headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    'Postman-Token': "0a18881c-ae1b-4b23-80d2-a62426a118b1"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+def generateReportUrl(reportId):
+    return "https://sprs.parl.gov.sg/search/sprs3topic?reportid=" + reportId
+
+def getReportIds(jsonObject):
+    reportIds = []
+    for entry in jsonObject:
+        reportIds.append(entry["reportId"])
+    return reportIds
+
+for reportId in getReportIds(response.json()):
+    print(generateReportUrl(reportId))
