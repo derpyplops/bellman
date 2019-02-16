33a94f44942006e0b00215126cc061fdf488381a
Kyle Tan <kyle@kyletan.com>
2019-02-16 09:04:23 +0000

    Fetches summarized data onto home page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from gevent.pywsgi import WSGIServer
 import settings as settings
 
+import json
+
 
 app = Flask(__name__)
 app.secret_key = settings.SECRET_KEY
@@ -9,7 +11,14 @@ app.secret_key = settings.SECRET_KEY
 
 @app.route("/")
 def home():
-    return "Hello world"
+    return render_template("home.html")
+
+@app.route("/q")
+def query():
+    page = request.args.get("page")
+    if page == "home":
+        data = json.loads(open(settings.HOME_DATA, "r").read())
+    return json.dumps(data)
 
 
 http_server = WSGIServer(("", settings.PORT), app)
